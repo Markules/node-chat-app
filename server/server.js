efa53547c152db15289ef7bb6cd0b89f84d1e277
Markules <markys.1988@gmail.com>
2020-08-12 09:05:40 +0300

    Add newMessagage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,8 +14,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+
+    socket.emit('newMessage', {
+        from: 'mark',
+        text: 'hey, this is Mark',
+        createdAt: 123
+    });
+
+
     socket.on('disconnect', () => {
         console.log('Client disconnected from server');
+    });
+
+    socket.on('createMessage', (newMessage) => {
+        console.log('received new message:', newMessage)
     });
 });
 
